84b0e4dadc0e3794cc8467c6018e661218110e04
Jeff Han <rainboww319@gmail.com>
2023-05-07 20:55:30 +0900

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int reverse(int N) {
+    // Several missing statements
+}
+
+int main() {
+    int N;
+    cout << "Please input an integer: ";
+    cin >> N;
+    cout << "Result: " << reverse(N) << endl;
+    return 0;
+}
